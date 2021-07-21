68bc1fd559a0f3de8b561849d089f44edec91b0a
Diego <diegoteixeirapereira@hotmail.com>
2021-07-21 09:41:25 -0300

    first commit

---
+++ src/main/java/br/com/appfrontreactandbackspring/AppfrontreactandbackspringApplication.java
@@ -0,0 +1,13 @@
+package br.com.appfrontreactandbackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfrontreactandbackspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfrontreactandbackspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/appfrontreactandbackspring/AppfrontreactandbackspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.appfrontreactandbackspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppfrontreactandbackspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
